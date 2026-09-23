f4d0183c4b62d2e215ce07610c02bf84233c0695
HT6FFAFL <hamza-externe.tariq@enedis.fr>
2026-09-23 15:43:48 +0200

    fix: correct 404 error when refreshing a page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
@@ -16,9 +16,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(MarkdownModule.forRoot()),
     provideHttpClient(),
-    provideRouter(routes, withInMemoryScrolling({
-      scrollPositionRestoration: 'top',
-    })),
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })
+    ),
     importProvidersFrom(
       TranslateModule.forRoot({
         defaultLanguage: 'en',
